b304c1c2892a817bf402c98670282f63fd6cbe98
Maxime Mikotajewski <mmikotajewski@witekio.com>
2019-04-26 12:38:40 +0200

    Moved sanity checks related to paths & filenames to a new file

--- add-lib-cmake-project.py
+++ add-lib-cmake-project.py
@@ -1,62 +1,11 @@
 #!/bin/python3
 
+
 from changedir import cd
-import os
 import sys
 import string
 import errno, os
-
-# Sadly, Python fails to provide the following magic number for us.
-ERROR_INVALID_NAME = 123
-
-def is_pathname_valid(pathname: str) -> bool:
-    '''
-    `True` if the passed pathname is a valid pathname for the current OS;
-    `False` otherwise.
-    '''
-    # If this pathname is either not a string or is but is empty, this pathname
-    # is invalid.
-    try:
-        if not isinstance(pathname, str) or not pathname:
-            return False
-
-        # Specific Windows processing
-        # Removes the Drive letter, which will be stored into "_", and keeps the rest in "pathname".
-        _, pathname = os.path.splitdrive(pathname)
-
-        # Directory guaranteed to exist. If the current OS is Windows, this is
-        # the drive to which Windows was installed (e.g., the "%HOMEDRIVE%"
-        # environment variable); else, the typical root directory.
-        root_dirname = os.environ.get('HOMEDRIVE', 'C:') if sys.platform == 'win32' else os.path.sep
-        assert os.path.isdir(root_dirname)   # ...Murphy and her ironclad Law
-
-        # Append a path separator to this directory if needed.
-        root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
-
-        # Test whether each path component split from this pathname is valid or
-        # not, ignoring non-existent and non-readable path components.
-        for pathname_part in pathname.split(os.path.sep):
-            try:
-                os.lstat(root_dirname + pathname_part)
-            except OSError as exc:
-                if hasattr(exc, 'winerror'):
-                    if exc.winerror == ERROR_INVALID_NAME:
-                        return False
-                elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
-                    return False
-    # If a "TypeError" exception was raised, it almost certainly has the
-    # error message "embedded NUL character" indicating an invalid pathname.
-    except TypeError as exc:
-        return False
-    else:
-        return True
-
-def isValidFilename(filename : str) -> bool:
-    validchars = set(string.ascii_letters + string.digits + string.punctuation.replace("/",""))
-    fn = set(filename)
-    print(validchars)
-    print(fn)
-    return filename != "." and filename != "/" and fn.issubset(validchars)
+from pathchecks import is_pathname_valid, isValidFilename 
 
 def argumentsValid(arg1 : str, arg2 : str) -> bool:
     if (os.path.isdir(arg1) and isValidFilename(arg2) and is_pathname_valid(arg1 + arg2)):
---
+++ pathchecks.py
@@ -0,0 +1,55 @@
+import string
+import errno
+import os
+import sys
+
+ERROR_INVALID_NAME = 123
+
+def is_pathname_valid(pathname: str) -> bool:
+    '''
+    `True` if the passed pathname is a valid pathname for the current OS;
+    `False` otherwise.
+    '''
+    # If this pathname is either not a string or is but is empty, this pathname
+    # is invalid.
+    try:
+        if not isinstance(pathname, str) or not pathname:
+            return False
+
+        # Specific Windows processing
+        # Removes the Drive letter, which will be stored into "_", and keeps the rest in "pathname".
+        _, pathname = os.path.splitdrive(pathname)
+
+        # Directory guaranteed to exist. If the current OS is Windows, this is
+        # the drive to which Windows was installed (e.g., the "%HOMEDRIVE%"
+        # environment variable); else, the typical root directory.
+        root_dirname = os.environ.get('HOMEDRIVE', 'C:') if sys.platform == 'win32' else os.path.sep
+        assert os.path.isdir(root_dirname)   # ...Murphy and her ironclad Law
+
+        # Append a path separator to this directory if needed.
+        root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
+
+        # Test whether each path component split from this pathname is valid or
+        # not, ignoring non-existent and non-readable path components.
+        for pathname_part in pathname.split(os.path.sep):
+            try:
+                os.lstat(root_dirname + pathname_part)
+            except OSError as exc:
+                if hasattr(exc, 'winerror'):
+                    if exc.winerror == ERROR_INVALID_NAME:
+                        return False
+                elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
+                    return False
+    # If a "TypeError" exception was raised, it almost certainly has the
+    # error message "embedded NUL character" indicating an invalid pathname.
+    except TypeError as exc:
+        return False
+    else:
+        return True
+
+def isValidFilename(filename : str) -> bool:
+    validchars = set(string.ascii_letters + string.digits + string.punctuation.replace("/",""))
+    fn = set(filename)
+    print(validchars)
+    print(fn)
+    return filename != "." and filename != "/" and fn.issubset(validchars)
