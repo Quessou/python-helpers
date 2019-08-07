aa5836848471dfb4e50c8ffa1a1245ef2aa2147f
Maxime Mikotajewski <mmikotajewski@witekio.com>
2019-08-07 14:50:38 +0200

    Added comment abou how to proceed to handle module installation

--- qssmodules/moduleloader.py
+++ qssmodules/moduleloader.py
@@ -14,6 +14,7 @@ pip_module_present = try_load('pip_api')
 
 if pip_module_present: # Here the assumption is made that modules have the same name when you install them than when you import them. Is that assumption wonky ?
     def try_install(module_name):
+        # We may want to consider what is said here : https://pip.pypa.io/en/latest/user_guide/#using-pip-from-your-program
         print(f'trying installing {module_name}')
 else:
     print("Please install pip_api module with following command line :\n\tpip install pip-api")
